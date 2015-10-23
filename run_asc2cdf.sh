38d512edb971685d805699f8fcb3d30639dc5aa4
rvalenzuelar <raul.valenzuela@colorado.edu>
2015-10-23 17:09:12 -0600

    added new case

--- run_asc2cdf.sh
+++ run_asc2cdf.sh
@@ -5,5 +5,6 @@
 #
 echo ' '
 # ./asc2cdf -d 2001-01-23 -v 010123I.std.ascii.raf 010123I.nc
-./asc2cdf -d 2001-01-25 -v 010125I.std.ascii.raf 010125I.nc
+# ./asc2cdf -d 2001-01-25 -v 010125I.std.ascii.raf 010125I.nc
+./asc2cdf -d 2001-02-17 -v 010217I.std.ascii.raf 010217I.nc
 echo ' '
