f17f971f2116a8ceb5c9a92425ea3585b4bbc90e
rvalenzuelar <raul.valenzuela@colorado.edu>
2016-01-15 16:17:49 -0700

    added new std files

--- run_asc2cdf.sh
+++ run_asc2cdf.sh
@@ -6,5 +6,9 @@
 echo ' '
 # ./asc2cdf -d 2001-01-23 -v 010123I.std.ascii.raf 010123I.nc
 # ./asc2cdf -d 2001-01-25 -v 010125I.std.ascii.raf 010125I.nc
-./asc2cdf -d 2001-02-17 -v 010217I.std.ascii.raf 010217I.nc
+# ./asc2cdf -d 2001-02-17 -v 010217I.std.ascii.raf 010217I.nc
+# ./asc2cdf -d 2001-02-09 -v 010209I.std.ascii.raf 010209I.nc
+# ./asc2cdf -d 2001-02-11 -v 010211I.std.ascii.raf 010211I.nc
+./asc2cdf -d 1998-01-18 -v 980118H.std.ascii.raf 980118H.nc
+./asc2cdf -d 1998-01-26 -v 980126H.std.ascii.raf 980126H.nc
 echo ' '
